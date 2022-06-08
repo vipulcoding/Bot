a0d767bcfb0441538ecfe135ea3b0021f7f83884
vipulcoding <106814460+vipulcoding@users.noreply.github.com>
2022-06-08 15:37:01 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,48 +1,45 @@
 var updateCallback = function(data){
-    var value = data.newValue;
-    console.log("after value ");
-    console.log("value : "+value);
-    var line = value[value.length -1];
-    console.log("line : "+line);
-    var movieName = line.text;
-	if (line.source.toLowerCase()==="visitor"){
-    var url = "https://www.omdbapi.com?t="+movieName+"&apikey=28df6aa3";
-    
-   
-    
-    fetch(url)
-    .then(function(response){
-    
-    return response.json();
-    }
-    ).then(function(res){
-    document.getElementById("Title").innerHTML = res.Title;
-    document.getElementById("Year").innerHTML = res.Year;
-    document.getElementById("Genre").innerHTML = res.Genre;
-    document.getElementById("Plot").innerHTML = res.Plot;
-	document.getElementById("Plot").innerHTML = res.Plot;
-    document.getElementById("Director").innerHTML = res.Director;
-    document.getElementById("Poster").innerHTML = res.Poster;
-    
-    
-    
-    }).catch(function(error){
-    console.log("Error Message : "+error);
-    })
-    }
-    };
-    
-    
-    
-    var notifyWhenDone = function(error) {
-    if (err){
-    console.log("Error Occured in notify : "+err);
-    }
-    
-    errorMessage.innerHTML = "Unable to find the movie";
-    };
-    
-    var chatText = "chatTranscript.lines";
-    
-    lpTag.agentSDK.init({});
-    lpTag.agentSDK.bind(chatText, updateCallback, notifyWhenDone);
+var value = data.newValue;
+console.log("after value ");
+console.log("value : "+value);
+var line = value[value.length -1];
+console.log("line : "+line);
+var movieName = line.text;
+if (line.source.toLowerCase()==="visitor"){
+var url = "https://www.omdbapi.com?t="+movieName+"&apikey=53e45af4";
+
+
+
+fetch(url)
+.then(function(response){
+
+return response.json();
+}
+).then(function(res){
+document.getElementById("Title").innerHTML = res.Title;
+document.getElementById("Year").innerHTML = res.Year;
+document.getElementById("Genre").innerHTML = res.Genre;
+document.getElementById("Plot").innerHTML = res.Plot;
+
+
+
+}).catch(function(error){
+console.log("Error Message : "+error);
+})
+}
+};
+
+
+
+var notifyWhenDone = function(error) {
+if (err){
+console.log("Error Occured in notify : "+err);
+}
+
+errorMessage.innerHTML = "Unable to find the movie";
+};
+
+var chatText = "chatTranscript.lines";
+
+lpTag.agentSDK.init({});
+lpTag.agentSDK.bind(chatText, updateCallback, notifyWhenDone);
